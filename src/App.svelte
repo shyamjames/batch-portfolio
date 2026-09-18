120bcc8549ea8f3d8b76647e3f2a8b6d1b51f366
Shyam James <shyamjames74@gmail.com>
2026-09-18 13:29:18 +0530

    feat: add App layout and Footer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,27 @@
 <script>
   import Router from 'svelte-spa-router'
   import Navbar from './components/Navbar.svelte'
+  import Footer from './components/Footer.svelte'
   import Toast  from './components/Toast.svelte'
   import { routes } from './router.js'
 </script>
 
-<Navbar />
-<main>
-  <Router {routes} />
-</main>
-<Toast />
+<div class="app-container">
+  <Navbar />
+  <main class="main-content">
+    <Router {routes} />
+  </main>
+  <Footer />
+  <Toast />
+</div>
 
 <style>
-  main {
-    min-height: calc(100vh - 60px);
+  .app-container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  .main-content {
+    flex: 1 0 auto;
   }
 </style>
